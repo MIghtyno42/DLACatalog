2943e515cdb38a125a292fcdc8a50700acb47acc
MIghtyno42 <michael.min@colorado.edu>
2017-04-18 15:43:17 -0600

    htmlMaker.py now works with new csv file
    This file now works with the csv file "2017.04.18_Project Listings DLA.csv". This new version ignores projects with no descriptions, adds the project websites (if provided), and places more information in the descriptions of each project, like supervision types and nature of work.

--- htmlMaker.py
+++ htmlMaker.py
@@ -3,11 +3,12 @@ import csv
 
 majorlist = ["ASEN","AREN","APPM","CHEN","CBEN","CSEN","CVEN","EEEN","ECEN","EPEN", "EVEN", "MCEN","TMEN", "GEEN"]
 realNames = ["Aerospace Engineering Sciences", "Architectural Engineering", "Applied Mathematics", "Chemical Engineering","Chemical and Biological Engineering", "Computer Science", "Civil Engineering", "Electrical Engineering", "Electrical and Computer Engineering", "Engineering Physics", "Environmental Engineering", "Mechanical Engineering","Technology, Arts and Media", "Engineering Plus"]
-columnNumbers = [16, 18, 17, 19, 20, 22, 21, 23, 24, 25, 27, 28, 29, 26]
+columnNumbers = [51, 53, 52, 54, 55, 57, 56, 58,59 , 60,62 , 63, 64, 61]
 
 position = 0
 for major in majorlist:	
-	with open("newfile.csv","r") as input_File:
+	with open("2017.04.18_Project Listings DLA.csv","r") as input_File:
+		next(input_File)
 		next(input_File)
 		fileString = major + "File.html"
 		with open (fileString,"w") as output_File:
@@ -33,30 +34,58 @@ for major in majorlist:
 			
 			for entry in listcsv:
 				
-				if (entry[columnNumbers[position]] != ""):
+				if (entry[columnNumbers[position]] != "" and entry[48] != ""):
 					output_File.write("\t\t\t<div id=\"wrapper\" style = \"width:100%;overflow:auto\">\n") 
-					titleString = "\t\t\t\t<h2 align = \"left\">"+ str(entry[13])+ "</h2>"
+					titleString = "\t\t\t\t<h2 align = \"left\">"+ str(entry[47])+ "</h2>"
 					output_File.write(titleString+"\n") 
 					output_File.write("\t\t\t\t<div style = \"width:20%; float:left;\">\n")
 					output_File.write("\t\t\t\t\t<h4 align = \"left\">Contact Info</h4>\n")
 					output_File.write("\t\t\t\t\t<p>\n")
-					output_File.write("\t\t\t\t\t\t"+str(entry[2])+"<br />\n")
-					output_File.write("\t\t\t\t\t\t"+str(entry[7])+"<br />\n")
-					output_File.write("\t\t\t\t\t\t"+str(entry[6])+"<br />\n")
-					output_File.write("\t\t\t\t\t\t"+str(entry[3])+"\n")
+
+					output_File.write("\t\t\t\t\t\t"+str(entry[14])+"<br />\n")
+					output_File.write("\t\t\t\t\t\t"+str(entry[27])+"<br />\n")
+					output_File.write("\t\t\t\t\t\t"+str(entry[25])+"<br />\n")
+					output_File.write("\t\t\t\t\t\t"+str(entry[26])+"\n")
 						
-					if (entry[10] != ""):
+					if (entry[33] != ""):
+						fullNameStr = entry[32] + " " + entry[33]
 						output_File.write("\t\t\t\t\t<p></p>\n")
-						output_File.write("\t\t\t\t\t\t"+str(entry[10])+"<br />\n")
-						output_File.write("\t\t\t\t\t\t"+str(entry[11])+"<br />\n")
-						output_File.write("\t\t\t\t\t\t"+str(entry[12])+"\n")
+						output_File.write("\t\t\t\t\t\t"+str(fullNameStr)+"<br />\n")
+						output_File.write("\t\t\t\t\t\t"+str(entry[34])+"<br />\n")
+						output_File.write("\t\t\t\t\t\t"+str(entry[35])+"\n")
 							
 					output_File.write("\t\t\t\t\t</p>\n")
 					output_File.write("\t\t\t\t</div>\n")
 					output_File.write("\t\t\t\t<div style=\"width:40%; float:right;\">\n")
 					output_File.write("\t\t\t\t\t<h4 align = \"left\">Description</h4>\n")
-					output_File.write("\t\t\t\t\t<p>"+str(entry[14])+"\n")
-					output_File.write("\t\t\t\t</p>\n")
+					if ("http" in entry [50] or "www" in entry[50] or "https" in entry[50] or ".com" in entry[50] or ".edu" in entry[50] or ".net" in entry[50] or ".gov" in entry[50]):
+						output_File.write("\t\t\t\t\t\t<a href = \"" + str(entry[50]) + "\">"+str(entry[50])+"\n")
+						output_File.write("\t\t\t\t</a>\n")			
+						
+					output_File.write("\t\t\t\t\t<ul style = \"list-style-type-disc\">\n")
+			
+					output_File.write("\t\t\t\t\t\t<li>"+str(entry[48])+"\n")
+
+					output_File.write("\t\t\t\t</li>\n")
+					if (entry[67] != ""):
+						if (entry[68] != ""):
+							output_File.write("\t\t\t\t\t\t<li>"+str(entry[67]) + ". " + str(entry[68])+"\n")
+							output_File.write("\t\t\t\t</li>\n")
+						else:
+							output_File.write("\t\t\t\t\t\t<li>"+str(entry[67])+"\n")
+							output_File.write("\t\t\t\t</li>\n")
+					if (entry[69] != ""):
+						if (entry[69] == "Other, specify:"):
+							output_File.write("\t\t\t\t\t\t<li> Nature of work: "+str(entry[69])+"\n")
+							output_File.write("\t\t\t\t</li>\n")
+						else:
+							if (entry[70] == ""):
+								output_File.write("\t\t\t\t\t\t<li> Nature of work: "+str(entry[69])+"\n")
+								output_File.write("\t\t\t\t</li>\n")
+							else:
+								output_File.write("\t\t\t\t\t\t<li> Nature of work: "+str(entry[69]) + ". " + str(entry[70])+"\n")
+								output_File.write("\t\t\t\t</li>\n")
+															
 					output_File.write("\t\t\t\t</div>\n")
 					output_File.write("\t\t\t\t<div style=\"width:38%;float:left;\">\n")
 					output_File.write("\t\t\t\t\t<h4 align = \"left\">Requirements</h4>\n")
@@ -64,12 +93,14 @@ for major in majorlist:
 					
 					
 					output_File.write("\t\t\t\t\t<p>\n")
-					output_File.write("\t\t\t\t\t\t"+str(entry[15])+"\n")
+					output_File.write("\t\t\t\t\t\t"+str(entry[49])+"\n")
+
 					output_File.write("\t\t\t\t\t</p>\n")
 					output_File.write("\t\t\t\t</div>\n")
 					output_File.write("\t\t\t</div>\n")
-					output_File.write("\t\t\t<hr style = \"height:1px;border:none;background-color:#333;\">\n")
 					output_File.write("\t\t\t<p align = \"center\"><a href = \"majorSelector.html\">Return to List of Majors</a></p>\n")
+					output_File.write("\t\t\t<hr style = \"height:1px;border:none;background-color:#333;\">\n")
+					
 					
 			output_File.write("\t</body>\n")
 			output_File.write("</html>\n")
